5ea44b1ab4e6bf37895ea483719280617837b96d
Teja Chilukuri <chilukuri.tejasrisai@gmail.com>
2025-01-10 11:57:56 -0600

    modified day1.py

--- Python/day1.py
+++ Python/day1.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+
+#Total 7 different types of operation are present
+"""
+1. Assignment operators(=, +=, -=, *=, /=, //=, %=)
+2. Arithmetic operators(+, -, *, /, //, **, %)
+3. Relational / Comparision operators(==, !=, <, >, <=, >=)
+4. Logical operators(and, or, not)
+5. increment and decrement operators(not in python) ++, --
+6. shift operators(<< , >>)
+7. Terinary operator(?)
+"""
+num=int(input("Enter a number: "))
+if(num % 2 == 0):
+    print("the given number is even")
+else:
+    print("The given number is odd")
