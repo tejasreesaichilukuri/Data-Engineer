7c151ac515e04a77da72748dace51d053eadf2de
Teja Chilukuri <chilukuri.tejasrisai@gmail.com>
2025-01-10 21:25:54 -0600

    modified.Tasks.py

--- Python/Tasks.py
+++ Python/Tasks.py
@@ -1,3 +1,4 @@
+"""
 print(1%12,end=" ")
 print(2%12,end=" ")
 print(3%12,end=" ")
@@ -29,3 +30,27 @@ print(27%12,end=" ")
 print(28%12,end=" ")
 print(29%12,end=" ")
 print(30%12,end=" ")
+"""
+"""
+Marks=int(input("Enter your marks: "))
+if(Marks >= 36 and Marks <= 100):
+    print("you are pass")
+else:
+    print("failed")
+"""
+
+#check the both number given are positive and atleast one of the number is greater than 10?
+"""
+num1=int(input("Enter number1: "))
+num2=int(input("Enter number2: "))
+positive= num1 > 0 and num2 > 0
+greater = num1 > 10 or num2 > 10
+Result= positive and greater
+print(Result)
+"""
+#you have 3users x,y,z  electricity bills check the sum of any two users should be always greater than 3bill?
+x=int(input("Enter the Electricity bill amount: "))
+y=int(input("Enter the Electricity bill amount: "))
+z=int(input("Enter the Electricity bill amount: "))
+condition= x+y > z and y+z > x and z+x > y
+print(condition)
